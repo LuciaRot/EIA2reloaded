51d9933f8cdf83acae78cdf78395ea291dcfc0be
LuciaRot <sweetlona3@gmail.com>
2021-03-26 15:31:48 +0100

    added css

--- L01_RandomPoem/RandomPoem.js
+++ L01_RandomPoem/RandomPoem.js
@@ -1,27 +1,38 @@
 "use strict";
 var Poem;
 (function (Poem) {
+    window.addEventListener("load", handleLoad);
+    let poem;
+    function handleLoad() {
+        poem = document.getElementById("poem");
+        document.querySelector("#btn1")?.addEventListener("mousedown", newPoem);
+        createPoem();
+    }
     let subjects = ["Harry", "Hermione", "Ron", "Hagrid", "Snape", "Dumbledore"];
     let verbs = ["braut", "liebt", "studiert", "hasst", "zaubert", "zerstört"];
     let objects = ["Zaubertränke", "den Grimm", "Lupin", "Hogwarts", "die Karte des Rumtreibers", "Dementoren"];
     // console.log(subjects);
     // console.log(verbs);
     // console.log(objects);
-    let poem = document.getElementById("poem");
-    for (let i = 5; i > 0; i--) {
-        let sentence = getVerse(subjects, verbs, objects);
-        // console.log(sentence);
-        if (poem) {
-            poem.innerHTML += sentence;
+    function createPoem() {
+        for (let i = 5; i > 0; i--) {
+            let sentence = getVerse(subjects, verbs, objects);
+            // console.log(sentence);
+            if (poem) {
+                poem.innerText += sentence;
+            }
+            console.log(poem);
         }
-        console.log(poem);
     }
     function getVerse(_subject, _verb, _object) {
         let randomSubject = Math.floor(Math.random() * subjects.length);
         let randomVerb = Math.floor(Math.random() * verbs.length);
         let randomObject = Math.floor(Math.random() * objects.length);
-        let verse = _subject.splice(randomSubject, 1)[0] + " " + _verb.splice(randomVerb, 1)[0] + " " + _object.splice(randomObject, 1)[0];
+        let verse = _subject.splice(randomSubject, 1)[0] + " " + _verb.splice(randomVerb, 1)[0] + " " + _object.splice(randomObject, 1)[0] + "\n";
         return verse;
+    }
+    function newPoem() {
+        location.reload();
     }
 })(Poem || (Poem = {}));
 //# sourceMappingURL=RandomPoem.js.map
--- L01_RandomPoem/RandomPoem.ts
+++ L01_RandomPoem/RandomPoem.ts
@@ -1,20 +1,31 @@
 namespace Poem {
+
+    window.addEventListener("load", handleLoad);
+    let poem: HTMLElement|null;
+
+    function handleLoad(): void {
+        poem = document.getElementById("poem");
+        document.querySelector("#btn1")?.addEventListener("mousedown", newPoem);
+        createPoem();
+    }
+
     let subjects: string[] = ["Harry", "Hermione", "Ron", "Hagrid", "Snape", "Dumbledore"];
     let verbs: string[] = ["braut", "liebt", "studiert", "hasst", "zaubert", "zerstört"];
     let objects: string[] = ["Zaubertränke", "den Grimm", "Lupin", "Hogwarts", "die Karte des Rumtreibers", "Dementoren"];
     // console.log(subjects);
     // console.log(verbs);
     // console.log(objects);
-    let poem: HTMLElement|null = document.getElementById("poem");
 
+    
+    function createPoem(): void {
     for (let i: number = 5; i > 0; i--) {
         let sentence: string = getVerse(subjects, verbs, objects);
         // console.log(sentence);
         if (poem) {
-            poem.innerHTML += sentence;
+            poem.innerText += sentence;
         }
         console.log(poem);
-    }
+    }}
 
     function getVerse(_subject: string[], _verb: string[], _object: string[]): string {
 
@@ -22,9 +33,13 @@ namespace Poem {
         let randomVerb: number = Math.floor(Math.random() * verbs.length);
         let randomObject: number = Math.floor(Math.random() * objects.length);
 
-        let verse: string = _subject.splice(randomSubject, 1)[0] + " " + _verb.splice(randomVerb, 1)[0] + " " + _object.splice(randomObject, 1)[0];
+        let verse: string = _subject.splice(randomSubject, 1)[0] + " " + _verb.splice(randomVerb, 1)[0] + " " + _object.splice(randomObject, 1)[0] + "\n";
 
         return verse;
 
+    }
+
+    function newPoem(): void {
+        location.reload();
     }
 }
