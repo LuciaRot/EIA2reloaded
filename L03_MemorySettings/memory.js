d941e3e563e7f1cfae72c2bb22c21844c23989f4
LuciaRot <sweetlona3@gmail.com>
2021-05-02 00:04:19 +0200

    memory

---
+++ L03_MemorySettings/memory.js
@@ -0,0 +1,55 @@
+"use strict";
+var memory;
+(function (memory) {
+    window.addEventListener("load", handleLoad);
+    let cards = [];
+    let tableTop;
+    let amount = 5;
+    let clicked = false;
+    let firstValue;
+    let secondValue;
+    let targetOne;
+    let targetTwo;
+    let time = 0;
+    let body = document.querySelector("body");
+    function handleLoad() {
+        let button = document.querySelector("button");
+        button.addEventListener("pointerdown", startGame);
+    }
+    function startGame() {
+        let fieldset = document.querySelector(".formular");
+        let hOne = document.querySelector("h1");
+        fieldset.classList.add("hidden");
+        hOne.classList.add("hidden");
+        tableTop = document.createElement("div");
+        document.body.appendChild(tableTop);
+        tableTop.classList.add("test");
+        createCards();
+    }
+    function createCards() {
+        for (let i = 1; i <= amount; i++) {
+            cards.push(i);
+            cards.push(i);
+        }
+        // console.log(cards);
+        for (let a = cards.length; a > 0; a--) {
+            let randomCard = Math.floor(Math.random() * cards.length);
+            let pickedCard = cards.splice(randomCard, 1);
+            console.log(pickedCard[0]);
+            tableTop = document.querySelector("div");
+            let card = document.createElement("div");
+            card.classList.add("visible");
+            card.id = pickedCard.toString();
+            //console.log(card.classList);
+            tableTop.appendChild(card);
+            card.innerText = pickedCard.toString();
+            card.addEventListener("pointerdown", turnCard);
+        }
+    }
+    function turnCard(_event) {
+        let target = _event.target;
+        console.log(target);
+        let value = ;
+    }
+})(memory || (memory = {}));
+//# sourceMappingURL=memory.js.map
---
+++ L03_MemorySettings/memory.ts
@@ -0,0 +1,66 @@
+namespace memory {
+
+    window.addEventListener("load", handleLoad);
+
+    let cards: number[] = [];
+    let tableTop: HTMLDivElement;
+    let amount: number = 5;
+    let clicked: boolean = false;
+    let firstValue: number;
+    let secondValue: number;
+    let targetOne: HTMLDivElement;
+    let targetTwo: HTMLDivElement;
+    let time: number = 0;
+    let body: HTMLElement = <HTMLElement>document.querySelector("body");
+
+
+    function handleLoad(): void {
+
+        let button: HTMLElement = <HTMLElement>document.querySelector("button");
+        button.addEventListener("pointerdown", startGame);
+    }
+
+    function startGame(): void {
+        let fieldset: HTMLFormElement = <HTMLFormElement>document.querySelector(".formular");
+        let hOne: HTMLElement = <HTMLElement>document.querySelector("h1");
+        fieldset.classList.add("hidden");
+        hOne.classList.add("hidden");
+
+        tableTop = <HTMLDivElement>document.createElement("div");
+
+        document.body.appendChild(tableTop);
+        tableTop.classList.add("test");
+
+        createCards();
+    }
+
+    function createCards(): void {
+        for (let i: number = 1; i <= amount; i++) {
+            cards.push(i);
+            cards.push(i);
+        }
+        // console.log(cards);
+
+        for (let a: number = cards.length; a > 0; a--) {
+            let randomCard: number = Math.floor(Math.random() * cards.length);
+            let pickedCard: number[] = cards.splice(randomCard, 1);
+            console.log(pickedCard[0]);
+
+            tableTop = <HTMLDivElement>document.querySelector("div");
+            let card: HTMLDivElement = <HTMLDivElement>document.createElement("div");
+            card.classList.add("visible");
+            card.id = pickedCard.toString();
+
+            //console.log(card.classList);
+            tableTop.appendChild(card);
+            card.innerText = pickedCard.toString();
+            card.addEventListener("pointerdown", turnCard);
+        }
+    }
+
+    function turnCard(_event: PointerEvent) {
+        let target: EventTarget = <EventTarget> _event.target;
+        console.log(target);
+        let value:  = ; 
+    }
+}
