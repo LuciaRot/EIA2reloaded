8d0d7cc3f30ac3e402a31777547c3309741978af
LuciaRot <sweetlona3@gmail.com>
2021-05-08 23:57:48 +0200

    new colors

--- L08.1_GenrativeKunst/GenerativeKunst.js
+++ L08.1_GenrativeKunst/GenerativeKunst.js
@@ -4,8 +4,8 @@ var GenerativeKunst;
     window.addEventListener("load", handleLoad);
     let canvas;
     let crc2;
-    let colors = ["#000000", "red", "blue", "purple", "orange"];
-    let amount = Math.floor(Math.random() * 10) + 5;
+    let colors = ["#dee1dd", "#c4cdc2", "#99aead", "#6d9197", "#658b6f", "#2f575d", "#28363d"];
+    let amount = Math.floor(Math.random() * 10) + 10;
     let scale = window.devicePixelRatio;
     function handleLoad() {
         document.querySelector(".btn")?.addEventListener("mousedown", reloadPage);
@@ -19,23 +19,40 @@ var GenerativeKunst;
     }
     function createImage() {
         for (let i = 0; i < amount; i++) {
-            let x = Math.floor(Math.random() * 150);
-            let y = Math.floor(Math.random() * 100);
+            let x = Math.floor(Math.random() * 600);
+            let y = Math.floor(Math.random() * 600);
             crc2.beginPath();
             crc2.moveTo(0, 0);
             crc2.lineTo(x, y);
             crc2.moveTo(x, y);
             crc2.lineTo(crc2.canvas.width, crc2.canvas.height);
             crc2.strokeStyle = colors[Math.floor(Math.random() * colors.length)];
+            crc2.globalAlpha = Math.random();
+            crc2.stroke();
+            crc2.closePath();
+        }
+        for (let i = 0; i < amount; i++) {
+            let x = Math.floor(Math.random() * 600);
+            let y = Math.floor(Math.random() * 600);
+            crc2.beginPath();
+            crc2.moveTo(crc2.canvas.width, 0);
+            crc2.lineTo(x, y);
+            crc2.moveTo(x, y);
+            crc2.lineTo(0, crc2.canvas.height);
+            crc2.strokeStyle = colors[Math.floor(Math.random() * colors.length)];
+            crc2.globalAlpha = Math.random();
             crc2.stroke();
             crc2.closePath();
         }
         for (let i = 0; i < 15; i++) {
-            let x = Math.floor(Math.random() * 200);
-            let y = Math.floor(Math.random() * 200);
+            let x = Math.floor(Math.random() * 600);
+            let y = Math.floor(Math.random() * 600);
             crc2.beginPath();
             crc2.arc(x, y, 20, 0, 2 * Math.PI);
             crc2.strokeStyle = colors[Math.floor(Math.random() * colors.length)];
+            crc2.fillStyle = colors[Math.floor(Math.random() * colors.length)];
+            crc2.fill();
+            crc2.globalAlpha = Math.random();
             crc2.stroke();
             crc2.closePath();
         }
--- L08.1_GenrativeKunst/GenerativeKunst.ts
+++ L08.1_GenrativeKunst/GenerativeKunst.ts
@@ -4,8 +4,8 @@ namespace GenerativeKunst {
 
     let canvas: HTMLCanvasElement;
     let crc2: CanvasRenderingContext2D;
-    let colors: string[] = ["#000000", "red", "blue", "purple", "orange"];
-    let amount: number = Math.floor(Math.random() * 10) + 5;
+    let colors: string[] = ["#dee1dd", "#c4cdc2", "#99aead", "#6d9197", "#658b6f", "#2f575d", "#28363d"];
+    let amount: number = Math.floor(Math.random() * 10) + 10;
     let scale: number = window.devicePixelRatio;
 
 
@@ -18,13 +18,13 @@ namespace GenerativeKunst {
         canvas.style.width = 600 + "px";
         canvas.style.height = 600 + "px";
         createImage();
-
+        
     }
 
     function createImage(): void {
         for (let i: number = 0; i < amount; i++) {
-            let x: number = Math.floor(Math.random() * 150);
-            let y: number = Math.floor(Math.random() * 100);
+            let x: number = Math.floor(Math.random() * 600);
+            let y: number = Math.floor(Math.random() * 600);
 
             crc2.beginPath();
             crc2.moveTo(0, 0);
@@ -32,16 +32,35 @@ namespace GenerativeKunst {
             crc2.moveTo(x, y);
             crc2.lineTo(crc2.canvas.width, crc2.canvas.height);
             crc2.strokeStyle = colors[Math.floor(Math.random() * colors.length)];
+            crc2.globalAlpha = Math.random();
+            crc2.stroke();
+            crc2.closePath();
+        }
+
+        for (let i: number = 0; i < amount; i++) {
+            let x: number = Math.floor(Math.random() * 600);
+            let y: number = Math.floor(Math.random() * 600);
+
+            crc2.beginPath();
+            crc2.moveTo(crc2.canvas.width, 0);
+            crc2.lineTo(x, y);
+            crc2.moveTo(x, y);
+            crc2.lineTo(0, crc2.canvas.height);
+            crc2.strokeStyle = colors[Math.floor(Math.random() * colors.length)];
+            crc2.globalAlpha = Math.random();
             crc2.stroke();
             crc2.closePath();
         }
 
         for (let i: number = 0; i < 15; i++) {
-            let x: number = Math.floor(Math.random() * 200);
-            let y: number = Math.floor(Math.random() * 200);
+            let x: number = Math.floor(Math.random() * 600);
+            let y: number = Math.floor(Math.random() * 600);
             crc2.beginPath();
             crc2.arc(x, y, 20, 0, 2 * Math.PI);
             crc2.strokeStyle = colors[Math.floor(Math.random() * colors.length)];
+            crc2.fillStyle = colors[Math.floor(Math.random() * colors.length)];
+            crc2.fill();
+            crc2.globalAlpha = Math.random();
             crc2.stroke();
             crc2.closePath();
         }
